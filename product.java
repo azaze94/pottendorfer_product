0df5e80aea69b45f761e4c275a91596e01e5ba8b
Max <pottendorfer.m2@gmail.com>
2021-04-02 19:32:32 +0200

    Max:Price Attribut bei Product.java hinzugeuegt

--- product.java
+++ product.java
@@ -1,21 +1,31 @@
 public class Product
 {
 	private String name;
+	private float price;
 
-	public Product(String name)
+	public Product(String name, float price)
 		{
 		setName(name);
+		setPrice(price);
 		}
 	public Product()
 		{
-		this("Coca-Cola")
+		this("Coca-Cola",10)
 		}
 	public String getName()
 		{
 		return name;
 		}
+	public float getPrice()
+		{
+		return price;
+		}
 	public  void setName(String name)
 		{
 		this.name = name;
+		}
+	public  void setPrice(float price)
+		{
+		this.price = price;
 		}
 }
